c68ede814c3aab2d847a33ad2ea6b957c8ee2e6f
Martin Galvan <martin.galvan@nordicsemi.no>
2024-01-08 12:51:25 +0100

    .

--- pynrfjprog/__init__.py
+++ pynrfjprog/__init__.py
@@ -3,31 +3,40 @@ Package marker file.
 
 """
 
-__version__ = "10.23.2" 
+__version__ = "10.23.4"
 
-import sys
 from pathlib import Path
+
+import platform
 import stat
 import logging
 
 
 def set_worker_executable(on_load=False):
-    os_name = sys.platform.lower()
-    if os_name.startswith("dar") or os_name.startswith("linux"):
-        # On linux and MacOS we need to make sure the jlinkarm worker is executable
+    os_name = platform.system().lower()
+
+    if os_name == 'linux' or os_name == 'darwin':
+        # On linux and MacOS we need to make sure the jlinkarm worker is executable.
         try:
-            for lib_path in Path(__file__).parent.glob("lib_*/jlinkarm_nrf_worker*"):
-                mode = lib_path.stat().st_mode
+            worker_bin_names = {
+                'linux': 'jlinkarm_nrf_worker_linux',
+                'darwin': 'jlinkarm_nrf_worker_osx'
+            }
+
+            worker_bin_name = worker_bin_names[os_name]
+
+            for worker_binary in Path(__file__).parent.glob(f"lib_*/{worker_bin_name}"):
+                mode = worker_binary.stat().st_mode
                 expected = stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+
                 if (mode & expected) != expected:
-                    logging.info(f"Mark {lib_path} as executable")
-                    lib_path.chmod(expected | mode)
+                    logging.info(f"Marking {worker_binary} as executable")
+                    worker_binary.chmod(expected | mode)
         except Exception:
             if on_load:
                 raise RuntimeError(
                     "jlinkarm_nrf_worker is not executable, and could not be made executable. \
                     Try doing this manually with python -m pynrfjprog.set_worker_executable"
                 )
-
 
 set_worker_executable(True)
